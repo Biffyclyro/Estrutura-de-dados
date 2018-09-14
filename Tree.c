83410c065dd62f5eaadbc01079eafdd9a5541d79
Biffyclyro <joao-senna1@hotmail.com>
2018-09-14 17:52:35 -0300

    Update Tree.c

--- Tree.c
+++ Tree.c
@@ -11,19 +11,47 @@ typedef struct tree Tree;
 Tree *enter(Tree *r, int x);
 void print(Tree *r);
 
+
+void imprime(Tree *r){
+	if(r!=NULL){
+		printf("%d\n", r->inf);
+		imprime(r->left);
+		imprime(r->right);		
+		
+	}
+	
+/*	if(r!=NULL){
+		
+		imprime(r->esq);
+		printf("%d\n", r->v);
+		imprime(r->dir);		
+		
+	}
+	
+		if(r!=NULL){
+		
+		imprime(r->esq);		
+		imprime(r->dir);
+		printf("%d\n", r->v);		
+		
+	}*/
+	
+	
+}
+
 main(){
 	Tree *r;
 	r=NULL;
 	r=enter(r,12);
 	r=enter(r,6);
 	r=enter(r,4);
-	print(r);
+	imprime(r);
 	
 	
 }
 
 Tree *enter(Tree *r, int x){
-	Tree *novo, *p;
+	Tree *novo, *p, *aux;
 	if(r==NULL){
 		novo=(Tree*) malloc(sizeof(Tree));
 		novo->inf=x;
@@ -31,9 +59,30 @@ Tree *enter(Tree *r, int x){
 		novo->right=r;
 		return novo;
 	}
+	p=r;
+	if(x<r->inf){
+		aux=p;
+		p=aux->left;
+	}else{
+		aux=p;
+		p=aux->right;
+	}
+	if(p==NULL){
+		novo=(Tree*) malloc(sizeof(Tree));
+		novo->inf=x;
+		novo->left=NULL;
+		novo->right=NULL;		
+		p=novo;
+		aux->		
+		return r;
+		
+	}else{
+		enter(p,x);
+	}
 	
 	
-	if(x < r->inf && r->left==NULL ){
+	
+/*	if(x < r->inf && r->left==NULL ){
 		//for(p=r; p->left!=NULL; p=p->left);
 		novo=(Tree*) malloc(sizeof(Tree));
 		r->left=novo;
@@ -46,7 +95,8 @@ Tree *enter(Tree *r, int x){
 		p=r->left;
 		enter(p, x);
 		
-	} /*
+	}
+	 
 	if(x > r->inf && r->right==NULL){
 		novo=(Tree*) malloc(sizeof(Tree));
 		r->right=novo;
@@ -63,6 +113,7 @@ Tree *enter(Tree *r, int x){
 }
 
 void print(Tree *r){
+	//preorder
 	Tree *p;
 	printf("%d\n", r->inf);
 	if(r->left!=NULL){
